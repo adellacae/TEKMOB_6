66bccaf1c968df664b0f9e2b2152a0ddd53b1cd0
adellacae <adellacaesardiniartika@gmail.com>
2024-07-01 12:00:16 +0700

    first commit

---
+++ lib/home.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+import 'model/product.dart';
+import 'model/products_repository.dart';
+import 'supplemental/asymmetric_view.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          icon: const Icon(
+            Icons.menu,
+            semanticLabel: 'menu',
+          ),
+          onPressed: () {},
+        ),
+        title: const Text('SHRINE'),
+        actions: <Widget>[
+          IconButton(
+            icon: const Icon(
+              Icons.search,
+              semanticLabel: 'search',
+            ),
+            onPressed: () {},
+          ),
+          IconButton(
+            icon: const Icon(
+              Icons.tune,
+              semanticLabel: 'filter',
+            ),
+            onPressed: () {},
+          ),
+        ],
+      ),
+      body: AsymmetricView(
+        products: ProductsRepository.loadProducts(Category.all),
+      ),
+      resizeToAvoidBottomInset: false,
+    );
+  }
+}
